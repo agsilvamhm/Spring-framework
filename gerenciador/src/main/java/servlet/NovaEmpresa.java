bc2a18f0dffae4d3fc2ffd7a473df1532cdcf9ec
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-13 22:07:25 -0300

    Comit do projeto

--- gerenciador/src/main/java/servlet/NovaEmpresa.java
+++ gerenciador/src/main/java/servlet/NovaEmpresa.java
@@ -16,12 +16,13 @@ import java.io.PrintWriter;
 public class NovaEmpresa extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nomeEmpresa = request.getParameter("nome");
 		System.out.println("Cadastrando uma nova empresa");
 		PrintWriter out = response.getWriter();
 		out.println("<Html>");
 		out.println("<body>");
-		out.println("<h1>Empresa cadastrada com sucesso!</h1>");
+		out.println("<h1>Empresa " + nomeEmpresa + " cadastrada com sucesso!</h1>" );
 		out.println("</body>");
 		out.println("</Html>");
 	}
