3c02adc958336f4095002b0d6ff6c7578a2660fe
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-16 20:33:42 -0300

    Comit para a aula de redirecionamento

--- gerenciador/src/main/java/servlet/NovaEmpresa.java
+++ gerenciador/src/main/java/servlet/NovaEmpresa.java
@@ -44,8 +44,10 @@ public class NovaEmpresa extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		RequestDispatcher rd =  request.getRequestDispatcher("/listaEmpresas");
-		request.setAttribute("empresa", empresa.getNome());
-		rd.forward(request, response);
+		response.sendRedirect("listaEmpresas");
+		
+//		RequestDispatcher rd =  request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 	}
 }
