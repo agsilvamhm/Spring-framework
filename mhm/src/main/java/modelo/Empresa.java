22f026b35d0b068f0279e38b6d24a17f77a71482
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-18 22:54:05 -0300

    Comit funcionando até o formulário nova empresa

---
+++ mhm/src/main/java/modelo/Empresa.java
@@ -0,0 +1,28 @@
+package modelo;
+
+import java.util.Date;
+
+public class Empresa {
+	private int id;
+	private String nome;
+	private Date dataAbertura = new Date();
+	
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
