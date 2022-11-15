754d3a027233e36794f4cf6992aa82a139cb1f44
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-11-15 11:45:26 -0300

    Gravando o projeto Loja-View

---
+++ loja-view/src/controller/CategoriaController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Categoria;
+
+public class CategoriaController {
+	
+
+	public List<Categoria> listar() {
+		List<Categoria> categorias = 
+				new ArrayList<Categoria>();
+		categorias.add(new Categoria(1, "Categoria de teste")); 
+		return categorias;
+	}
+}
