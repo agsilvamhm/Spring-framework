f6b46dbd42c8885afb1ebd2b042b13cd38f39ea0
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-11-17 21:20:24 -0300

    Registrando o mapeamento de tabelas

--- loja/src/main/java/br/com/alura/loja/modelo/Produto.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,11 +1,15 @@
 package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +21,20 @@ public class Produto {
 	private String nome;
 	private String descricao;
 	private BigDecimal preco;
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@ManyToOne
+	private Categoria categoria;
+	
+	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.categoria = categoria;
+	}
+
+	public Produto() {	
+	}
 	
 	public Long getId() {
 		return id;
--- loja/src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/teste/CadastroDeProduto.java
@@ -6,23 +6,25 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi");
-		celular.setDescricao("Muit legal");
-		celular.setPreco(new BigDecimal("800"));
-		
+		Categoria celulares = new Categoria("Celulares");
+		Produto celular = new Produto("Xiaomi","Muit legal",new BigDecimal("800"), celulares );
+	
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDao dao = new ProdutoDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
-		dao.cadastrar(celular);
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);	
 		em.getTransaction().commit();
 		em.close();
 	}
